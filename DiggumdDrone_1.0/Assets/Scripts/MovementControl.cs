c38fefa85b5d5101cd438d70d14f6266675847de
earlbalageo01 <45143597+earlbalageo01@users.noreply.github.com>
2020-01-29 02:43:59 -0800

    Movements
    Movements are good (I think), feel free to find bugs, lots of warnings on the console tho. but all good

---
+++ DiggumdDrone_1.0/Assets/Scripts/MovementControl.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MoonSharp.Interpreter;
+
+public class MovementControl : MonoBehaviour
+{
+    public GameObject drone;
+    Vector3 dronepos;
+    float facingDirection; //stores where the drone is facing by angle
+
+
+    //moves one block towards the drone facing.
+    void forward(){
+
+        if (facingDirection == 0) {
+            dronepos.z += 1f;
+            drone.transform.position = dronepos;
+        }
+        else if(facingDirection == 90 || facingDirection == -270){
+            dronepos.x += 1f;
+            drone.transform.position = dronepos;
+        }
+        else if (facingDirection == -90 || facingDirection == 270 ){
+            dronepos.x -= 1f;
+            drone.transform.position = dronepos;
+        }
+        else if(facingDirection == 180 || facingDirection == -180){
+            dronepos.z -= 1f;
+            drone.transform.position = dronepos;
+        }
+    }
+
+    //Rotates drone to the right
+    void turnRight(){
+        facingDirection += 90;
+        
+        drone.transform.Rotate(0,90,0,Space.World);
+    }
+
+    //rotates drone to the left
+    void turnLeft(){
+        facingDirection -= 90;
+        drone.transform.Rotate(0,-90,0,Space.World);
+        Debug.Log(facingDirection);
+    }
+
+
+    public void Move(string sequence){
+    if(facingDirection == 360 || facingDirection == -360) facingDirection = 0;
+
+        switch (sequence)
+        {
+            case "f":
+                forward();
+                break;
+            case "r":
+                turnRight();
+                break;
+            case "l":
+                turnLeft();
+                break;
+            default: break;
+        }
+    }
+
+
+    void Start()
+    {
+        
+        
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        dronepos = drone.transform.position; //stores the location of the drone
+    }
+
+
+}
