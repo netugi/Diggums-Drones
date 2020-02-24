698b68deba116ef563428f338ea0c30fa2b53702
Amlan Nag <43157787+amlannag6@users.noreply.github.com>
2020-02-24 13:22:20 -0800

    With New texture and underground
    New update

---
+++ Assets/Scripts/LevelSelectControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectControl : MonoBehaviour
+{
+	
+	public void SelectLevel(string _scene)
+	{
+		SceneManager.LoadScene(_scene);
+	}
+}
