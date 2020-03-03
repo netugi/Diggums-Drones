8480171c49b441378ca14c1136ff082dca28202e
Oskar Jensen <taraquad@gmail.com>
2020-03-02 18:58:49 -0800

    Added a main menu UI along with a splash screen
    Main menu contains level difficulty categories to choose from, along with an expandable side panel containing buttons for login, register, logout, settings, and exit. None of these are functional in this version.

---
+++ Assets/Scripts/SplashTransition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashTransition : MonoBehaviour
+{
+	public Image blanket;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FadeSplash());
+    }
+	
+	IEnumerator FadeSplash()
+	{
+		blanket.canvasRenderer.SetAlpha(0f);
+		blanket.CrossFadeAlpha(1f, 1.5f, false);
+		yield return new WaitForSeconds(2.5f);
+		blanket.CrossFadeAlpha(0f, 1.5f, false);
+		yield return new WaitForSeconds(1.5f);
+		SceneManager.LoadScene("MainMenu");
+	}
+}
